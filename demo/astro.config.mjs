ade0a33dec238794aa7ba32fa3d3b09933d49bb4
Ayo <ayo@ayco.io>
2026-06-14 21:17:07 +0200

    chore: fix demo

--- demo/astro.config.mjs
+++ demo/astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config'
 // https://astro.build/config
 export default defineConfig({
 	image: {
-		// domains: ['github-readme-stats.vercel.app'],
+		domains: ['github-readme-stats.vercel.app'],
 	},
 })
